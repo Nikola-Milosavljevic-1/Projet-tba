f6f6071ac446416e38413c9d5f2a5926d7801258
Nikola-Milosavljevic-1 <nikola.milosavljevic@edu.esiee.fr>
2024-12-06 10:23:41 +0000

    test

--- player.py
+++ player.py
@@ -25,7 +25,7 @@ class Player():
             print("\nAucune porte dans cette direction !\n")
             return False
         
-        if next_room == "mort":
+        if next_room == "mort":  #si dans la prochaine pièce est "mort", alors afficher qu'il est mort et qu'il a perdu
             print("\nC'ètait un piège, une porte qui menait dans le vide! Vous êtes de mort de chute\n")
             return False
   
@@ -38,14 +38,14 @@ class Player():
     
     def get_history(self):
         
-        if not self.history_room:
+        if not self.history_room:   # Ici on verifie si la pile est vide
             print("\nAucune pièce n'a encore été visitée.")
-            return []
+            return False #indique bien que le retour en arrière a été un echec
 
-        history = [room.name for room in self.history_room]
+        history = [room.name for room in self.history_room] # Crée une liste des noms des pièces dans la pile history_room
         print("\nHistorique des pièces visitées :")
-        for room_name in history:
-            print(f"- {room_name}")
+        for room_name in history: #Parcourt chaque nom de pièce dans la liste history
+            print(f"- {room_name}") # Affiche chaque nom de pièce avec un tiret devant
         return history
 
     def back(self):
@@ -53,7 +53,7 @@ class Player():
             print("\nImpossible de revenir en arrière. Vous êtes dans la salle de départ !")
             return False
         
-         previous_room=self.history_room.pop()
+         previous_room=self.history_room.pop() #retirer et retourner le dernier élément         
          self.current_room=previous_room
          print(f"\nVous êtes retourné à la salle précédente : {self.current_room.name}")
          print(self.current_room.get_long_description())
